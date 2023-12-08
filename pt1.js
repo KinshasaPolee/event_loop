0a420d06f228d9b31718ec268e2826e7a289751c
KinshasaPolee <kpolee1@gmail.com>
2023-12-08 05:47:35 -0500

    pt1 and pt2 added

---
+++ pt1.js
@@ -0,0 +1,26 @@
+// Part 1: Stack Overflow
+var counter = 0;
+
+function incrementAndCall() {
+    counter++;
+
+    if(counter >=== 1500) {
+
+        console.log('Error condition reached!');
+        return;
+    }
+
+    try {
+
+        incrementAndCall();
+
+    } catch (error) {
+
+        console.error('Error: ${error.message}');
+        console.log('Counter Value: ${counter}');
+    } finally {
+
+    }
+}
+
+incrementAndCall();
