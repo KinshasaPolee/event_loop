5b206479108ba805bf04c0f0dea3152e07de4898
KinshasaPolee <kpolee1@gmail.com>
2023-12-08 19:48:21 -0500

    pt.3 completed

--- pt3.js
+++ pt3.js
@@ -1,1 +1,44 @@
 // Part 3: Deferred Execution
+document.addEventListener('DOMContentLoaded', function() {
+
+    const primeNumbersContainer = document.getElementById('primeNumbersContainer');
+
+    function isPrime(num) {
+
+    for (let i = 2; i < num; i++) {
+
+        if (num % i === 0) {
+
+        return false;
+        }
+    }
+    return num > 1;
+    }
+
+        function addPrimeNumbersToContainer(n) {
+
+    let primes = [];
+    for (let i = 1; i <= n; i++) {
+
+        if (isPrime(i)) {
+
+        primes.push(i);
+        }
+    }
+
+    const ul = document.createElement('ul');
+    primes.forEach(prime => {
+
+        const li = document.createElement('li');
+        li.textContent = prime;
+        ul.appendChild(li);
+    });
+
+    primeNumbersContainer.innerHTML = '';
+    primeNumbersContainer.appendChild(ul);
+    }
+
+    addPrimeNumbersToContainer(20);
+
+    alert('Prime number calculation is complete!');
+});
