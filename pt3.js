13d6851005f565b11817ead290a5be68b32621a1
KinshasaPolee <kpolee1@gmail.com>
2023-12-08 20:42:36 -0500

    modified

--- pt3.js
+++ pt3.js
@@ -3,42 +3,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const primeNumbersContainer = document.getElementById('primeNumbersContainer');
 
-    function isPrime(num) {
-
+        function isPrime(num) {
     for (let i = 2; i < num; i++) {
-
         if (num % i === 0) {
-
         return false;
         }
     }
     return num > 1;
     }
 
-        function addPrimeNumbersToContainer(n) {
+    function renderPrimesIncrementally(n) {
+    let count = 0;
 
-    let primes = [];
-    for (let i = 1; i <= n; i++) {
+    function renderNextPrime() {
+        if (count <= n) {
+        if (isPrime(count)) {
+        
+            primeNumbersContainer.textContent = `Current prime number: ${count}`;
+        }
+        count++;
 
-        if (isPrime(i)) {
-
-        primes.push(i);
+        setTimeout(renderNextPrime, 0);
+        } else {
+    
+        alert('Prime number calculation is complete!');
         }
     }
 
-    const ul = document.createElement('ul');
-    primes.forEach(prime => {
-
-        const li = document.createElement('li');
-        li.textContent = prime;
-        ul.appendChild(li);
-    });
-
-    primeNumbersContainer.innerHTML = '';
-    primeNumbersContainer.appendChild(ul);
+    renderNextPrime();
     }
 
-    addPrimeNumbersToContainer(20);
-
-    alert('Prime number calculation is complete!');
+    renderPrimesIncrementally(10000);
 });
